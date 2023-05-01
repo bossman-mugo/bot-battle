c5e89377b8a9e066e4bd60413b3d469388d09a68
Eric Mugo Thiong'o <mugoericpg20@gmail.com>
2023-05-01 21:40:47 +0300

    Exported SortBar component

---
+++ src/components/SortBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const SortBar = ({sortOption, onSortOptionChange}) => {
+    
+    return (
+        <div>
+            <label htmlFor="sort-se;ect">Sort Bots By: </label>
+            <select id="sort-select" value={sortOption} onChange={handleChnage}>
+                <option value="health">Health</option>
+                <option value="damage">Damage</option>
+                <option value="armor">Armor</option>
+            </select>
+        </div>
+    )
+}
+
+export default SortBar;
