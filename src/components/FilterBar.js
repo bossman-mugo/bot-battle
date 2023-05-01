074ea9ce84bc1ab1e8214cbcdfdad7ed8f147dca
Eric Mugo Thiong'o <mugoericpg20@gmail.com>
2023-05-01 23:07:09 +0300

    Added classOptions

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -2,6 +2,10 @@ import React from "react";
 
 const FilterBar = ({filters, onFilterClass, onRemoveFilter}) => {
 
+    const classOptions = ["Support", "Medic", "Assault", "Defender", "Captain", "Witch"];
+
+    
+
 return (
     <div>
         <h3>Classes</h3>
