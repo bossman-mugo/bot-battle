bc64b51be97f743336c47d45fe6a2684173e6eb4
Eric Mugo Thiong'o <mugoericpg20@gmail.com>
2023-05-01 23:32:03 +0300

    Render BotCard on BotSpecs

---
+++ src/components/BotSpecs.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BotCard from "./BotCard";
+
+
+const BotSpecs = ({bot, onBackToCollection, onEnlistBot}) => {
+
+    return (
+        <div classname="bot-specs">
+            <BotCard bot={bot} onSelect={null} />
+            <button onClick={() => onEnlistBot(bot)}>Enlist Bot</button>
+            <button onClick={onBackToCollection}>Back to Collection</button>
+        </div>
+    )
+}
