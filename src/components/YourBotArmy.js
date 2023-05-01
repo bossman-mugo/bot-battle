8a2e4244241bd1a93dd2b1bd552ddda0ad5c457e
Eric Mugo Thiong'o <mugoericpg20@gmail.com>
2023-05-01 21:20:26 +0300

    imported BotCard into YourBotArmy

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,11 +1,14 @@
 import React from "react";
+import BotCard from "./BotCard";
 
 
 const YourBotarmy = ({bots, onrReleaseBot, onDeleteBot, onSelectBot}) => {
 
+    const botCards = 
 
 
-    
+
+
 return (
     <div className="bot-army">
         <h2>Your Bot Army</h2>
