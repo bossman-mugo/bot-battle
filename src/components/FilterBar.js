d31332182a586187203a67d62664fb9204176d2a
Eric Mugo Thiong'o <mugoericpg20@gmail.com>
2023-05-01 23:00:38 +0300

    Exported FilterBar

---
+++ src/components/FilterBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const FilterBar = ({filters, onFilterClass, onRemoveFilter}) => {
+
+return (
+    <div>
+        <h3>Classes</h3>
+    </div>
+)
+
+}
+
+
+export default FilterBar;
