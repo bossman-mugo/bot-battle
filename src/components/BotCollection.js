559a48a81620f2df11618681656f3db9d90a4d01
Eric Mugo Thiong'o <mugoericpg20@gmail.com>
2023-05-02 15:45:59 +0300

    Commented BotCollection

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,14 +1,23 @@
 import React from "react";
+import BotCard from "./BotCard";//import the BotCard component from BotCard
 
+
+//component that takes in props such as bots
 const BotCollection = ({bots, onEnlistBot, onSelectBot}) => {
+
+     //create an array of bot components as it maps every bot in the bots prop
+    // the bot prop represents the current bot
+    
     const botCards = bots.map(bot => <BotCard key={bot.id} bot={bot} buttonText="Enlist Bot" onClick={() => onEnlistBot(bot)} onSelect={() => onSelectBot(bot)}/>)
 
+
+    // return div that renders conditionally 
     return (
         <div className="bot-collection">
             <h2>
                 Bots Available for Enlisting
             </h2>
-            {bots.length . 0? botCards : <p> No bots available. </p>}
+            {bots.length = 0? botCards : <p> No bots available. </p>}
         </div>
     )
 
